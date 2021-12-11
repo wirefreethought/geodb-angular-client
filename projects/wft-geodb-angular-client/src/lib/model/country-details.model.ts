e3637aaba38c240d96c329262a597ff2bd9f011b
Michael Mogley <mrrubato@gmail.com>
2021-12-11 12:40:06 -0800

    Add CountryDetails.callingCode field

--- projects/wft-geodb-angular-client/src/lib/model/country-details.model.ts
+++ projects/wft-geodb-angular-client/src/lib/model/country-details.model.ts
@@ -1,6 +1,7 @@
 import {CountrySummary} from './country-summary.model';
 
 export class CountryDetails extends CountrySummary {
+  callingCode: string;
   flagImageUri: string;
   numRegions: number;
 }
